740f43e97498e37b201d6a3bdcf130b6215aecde
Luke <luke@luke.co.pl>
2016-08-23 20:13:53 +0200

    Make use of Uglify's "toplevel mangling", correct minified file name

--- 1234.js
+++ 1234.js
@@ -1,66 +1,82 @@
-((document, requestAnimationFrame, width, height, playerPosX, playerPosY, playerVx, playerVy, jumpVy, gravity, circleRadius, enemies, startTimestamp) => {
-    s = 0;
-    setInterval(() => {
-        s = parseInt((new Date() - startTimestamp) / 1000);
-    }, 1000);
-    document.body.style.background='#333';
-    document.body.innerHTML=`<canvas id="_" width="${width}" height="${height}" style="background:white;position:absolute;top:50%;left:50%;margin:-${height/2}px 0 0 -${width/2}px"></canvas>`;
-    c = _.getContext('2d');
-    k={}
-    document.onkeydown = (e) => {
-        k[e.keyCode] = true;
+let d = document,
+    width = 800,
+    height = 300,
+    playerPosX = 400,
+    playerPosY = 100,
+    circleRadius = 20,
+    maxPosTop = circleRadius,
+    maxPosRight = width - circleRadius,
+    maxPosBottom = height - circleRadius,
+    maxPosLeft = circleRadius,
+    playerVx = 0,
+    playerVy = 0,
+    jumpVy = 10,
+    gravity = 0.2,
+    enemies = [],
+    startTimestamp = +new Date(),
+    fullSecondsElapsed = 0,
+    keyStates = {},
+    context,
+    drawingLoop;
+
+setInterval(() => {
+    fullSecondsElapsed = parseInt((new Date() - startTimestamp) / 1000);
+}, 1000);
+
+d.body.style.background='#333';
+d.body.innerHTML=`<canvas id="_" width="${width}" height="${height}" style="background:white;position:absolute;top:50%;left:50%;margin:-${height/2}px 0 0 -${width/2}px"></canvas>`;
+context = _.getContext('2d');
+
+d.onkeydown = (e) => {
+    keyStates[e.keyCode] = true;
+}
+d.onkeyup = (e) => {
+    keyStates[e.keyCode] = false;
+}
+
+setInterval(() => {
+    if(keyStates[37]) {
+        playerVx -= 1;
     }
-    document.onkeyup = (e) => {
-        k[e.keyCode] = false;
+    if(keyStates[39]) {
+        playerVx += 1
     }
-    /* top, right, bottom, left min/max circle positions */
-    t = circleRadius;
-    r = width - circleRadius;
-    b = height - circleRadius;
-    l = circleRadius;
-    setInterval(() => {
-        if(k[37]) {
-            playerVx -= 1;
-        }
-        if(k[39]) {
-            playerVx += 1
-        }
 
-        if(playerVx<-10) playerVx=-10;
-        if(playerVy>10) playerVx=10;
+    if(playerVx < -10) playerVx = -10;
+    if(playerVy > 10) playerVx = 10;
 
-        playerVx *= 0.9;
-        playerPosX += playerVx;
-        if(playerPosX <= l) {
-            playerPosX = l;
-            playerVx = -playerVx;
-        }
-        if(playerPosX >= r) {
-            playerPosX = r;
-            playerVx = -playerVx;
-        }
-
-        if(k[38] && playerPosY==b) {
-            playerVy = jumpVy;
-        }
-
-        playerVy -= gravity;
-        playerPosY -= playerVy;
-        if(playerPosY >= b) {
-            playerPosY = b;
-            playerVy = 0.8 * -playerVy;
-        }
-    }, 15);
-    f='fillStyle';
-    d = () => {
-        _.width = _.width;
-        c[f] = '#8f8';
-        c.arc(playerPosX, playerPosY, circleRadius, 0, 2 * Math.PI);
-        c.fill();
-        c.font = '24px monospace'
-        c[f] = 'black';
-        c.fillText(s, 10, 28);
-        requestAnimationFrame(d);
+    playerVx *= 0.9;
+    playerPosX += playerVx;
+    if(playerPosX <= maxPosLeft) {
+        playerPosX = maxPosLeft;
+        playerVx = -playerVx;
     }
-    d();
-})(document, requestAnimationFrame, 800, 300, 400, 100, 0, 0, 10, 0.2, 20, [], +new Date())
+    if(playerPosX >= maxPosRight) {
+        playerPosX = maxPosRight;
+        playerVx = -playerVx;
+    }
+
+    if(keyStates[38] && playerPosY == maxPosBottom) {
+        playerVy = jumpVy;
+    }
+
+    playerVy -= gravity;
+    playerPosY -= playerVy;
+    if(playerPosY >= maxPosBottom) {
+        playerPosY = maxPosBottom;
+        playerVy = 0.8 * -playerVy;
+    }
+}, 15);
+
+f='fillStyle';
+drawingLoop = () => {
+    _.width = _.width;
+    context[f] = '#8f8';
+    context.arc(playerPosX, playerPosY, circleRadius, 0, 2 * Math.PI);
+    context.fill();
+    context.font = '24px monospace'
+    context[f] = 'black';
+    context.fillText(fullSecondsElapsed, 10, 28);
+    requestAnimationFrame(drawingLoop);
+}
+drawingLoop();
